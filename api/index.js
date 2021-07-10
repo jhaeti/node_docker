2a51dad85047daac49a5127335003c866e1e0b3a
unknown <jhaeti@gmail.com>
2021-07-10 20:35:18 +0000

    Added mongo database feature

--- api/index.js
+++ api/index.js
@@ -1,10 +1,28 @@
 const express = require("express");
+const mongoose = require("mongoose");
+
 const app = express();
 
 const port = 3000;
 
+const uri = "mongodb://mongo:27017/nodeDocker";
+
+mongoose.set("useCreateIndex", true);
+mongoose.connect(
+  uri,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  },
+  () => console.log("MongoDb connected...")
+);
+
 app.get("/", (req, res) => {
   res.send("<h1>From Node Server</h1>");
 });
+
+app.use(express.json({ extended: false }));
+
+app.use("/api", require("./route"));
 
 app.listen(port, console.log(`Server running on port ${port}`));
