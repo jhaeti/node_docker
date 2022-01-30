97aba44205be85aa2bc75dd7b4efc635b71f7c49
unknown <jhaeti@gmail.com>
2022-01-30 23:51:54 +0000

    Added some scripts for testing

--- api/index.js
+++ api/index.js
@@ -1,28 +1,19 @@
-const express = require("express");
-const mongoose = require("mongoose");
+// const mongoose = require("mongoose");
 
-const app = express();
+const app = require("./app");
 
 const port = 3000;
 
-const uri = "mongodb://mongo:27017/nodeDocker";
+// const uri = "mongodb://mongo:27017/nodeDocker";
 
-mongoose.set("useCreateIndex", true);
-mongoose.connect(
-  uri,
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  },
-  () => console.log("MongoDb connected...")
-);
-
-app.get("/", (req, res) => {
-  res.send("<h1>From Node Server</h1>");
-});
-
-app.use(express.json({ extended: false }));
-
-app.use("/api", require("./route"));
+// mongoose.set("useCreateIndex", true);
+// mongoose.connect(
+//   uri,
+//   {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+//   },
+//   () => console.log("MongoDb connected...")
+// );
 
 app.listen(port, console.log(`Server running on port ${port}`));
