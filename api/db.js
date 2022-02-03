06cda71d3fbc3888cb6ee201550c1409fd23c0b3
unknown <jhaeti@gmail.com>
2022-02-03 07:40:29 +0000

    Separated db file

---
+++ api/db.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+
+const uri =
+    process.env.NODE_ENV == "development"
+        ? "mongodb://localhost:27017/nodeDocker"
+        : "mongodb://localhost:27017/nodeDocker-test";
+
+mongoose.set("useCreateIndex", true);
+mongoose.connect(
+    uri,
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    },
+    () => console.log("MongoDb connected...")
+);
