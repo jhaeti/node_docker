0e4e751466837ec99cc87fee5c44450021fa234a
unknown <jhaeti@gmail.com>
2022-02-04 16:59:48 +0000

    Updated to run using the mongo service in database

--- api/db.js
+++ api/db.js
@@ -3,6 +3,8 @@ const mongoose = require("mongoose");
 const uri =
     process.env.NODE_ENV == "development"
         ? "mongodb://localhost:27017/nodeDocker"
+        : process.env_NODE_ENV == "production"
+        ? "mongodb://mongo:27017/nodeDocker"
         : "mongodb://localhost:27017/nodeDocker-test";
 
 mongoose.set("useCreateIndex", true);
