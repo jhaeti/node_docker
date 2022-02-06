b254767b74b7dbed547fdae2840f5d6269a061db
unknown <jhaeti@gmail.com>
2022-02-06 03:43:06 +0000

    Back to server Two

--- api/app.js
+++ api/app.js
@@ -8,7 +8,7 @@ app.use(express.json({ extended: false }));
 app.use("/api", require("./router"));
 
 app.get("/", (req, res) => {
-    res.send("<h1>From Node Serveruuuuuuuuu</h1>");
+    res.send("<h1>From Node Server LAST FOR 2DAY</h1>");
 });
 
 module.exports = app;
