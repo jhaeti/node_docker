924982d7f9a956c59fb2fd3d194de12921044ad3
unknown <jhaeti@gmail.com>
2022-02-06 09:03:37 +0000

    Fixed Note deleting dangling image bug

--- api/app.js
+++ api/app.js
@@ -8,7 +8,9 @@ app.use(express.json({ extended: false }));
 app.use("/api", require("./router"));
 
 app.get("/", (req, res) => {
-    res.send("<h1>From Node Server LAST FOR 2DAY</h1>");
+    res.send(
+        "<h1>From Node Server. THANK ALLAH EVERY THING IS GOING ON WELL</h1>"
+    );
 });
 
 module.exports = app;
