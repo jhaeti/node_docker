fecd71e532613f6be0e3c92aac58344b4439f07f
unknown <jhaeti@gmail.com>
2022-02-12 13:58:08 +0000

    MODIFIED SERVER.JS

--- api/app.js
+++ api/app.js
@@ -8,7 +8,7 @@ app.use(express.json({ extended: false }));
 app.use("/api", require("./router"));
 
 app.get("/", (req, res) => {
-    res.send("<h1>Now Every thing works</h1>");
+    res.send("<h1>Now Every thing WORKS</h1>");
 });
 
 module.exports = app;
