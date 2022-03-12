9e02c3bb982d35e5c8dd833e843907aa58fa5208
unknown <jhaeti@gmail.com>
2022-03-12 14:06:03 +0000

    Everything should work

--- api/app.js
+++ api/app.js
@@ -1,14 +1,15 @@
 const express = require("express");
 const app = express();
-
+require("dotenv").config();
 // require("./db");
 
+const bookName = process.env.BOOK_NAME;
 app.use(express.json({ extended: false }));
 
 app.use("/api", require("./router"));
 
 app.get("/", (req, res) => {
-    res.send("<h1>Now Every thing WORKS</h1>");
+    res.send(`<h1>Now Every thing WORKS.<br> ${bookName}</h1>`);
 });
 
 module.exports = app;
